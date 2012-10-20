9b3a62a137b4cbde35a9d09d43a0dc10aa42fb42
Mosè Giordano <giordano.mose@libero.it>
2012-10-20 23:49:04 +0200

    particlessymb.el: no arguments for commands that don't need them

--- particlessymb.el
+++ particlessymb.el
@@ -146,537 +146,536 @@
      '("PNzP" 1)
      '("PNpP" 1)
      '("PNP" 1)
-     "upvarphi"
-     "Pg"
-     "PGg"
-     "PW"
-     "PWpm"
-     "PWmp"
-     "PWp"
-     "PWm"
-     "PZ"
-     "PZz"
-     "PH"
-     "PHz"
-     "PWDt"
-     "PWpDt"
-     "PWpr"
-     "PWL"
-     "PWR"
-     "PZpr"
-     "PZprpr"
-     "PZst"
-     "PZzDt"
-     "PZzDT"
-     "PZL"
-     "PZLR"
-     "PZR"
-     "PZGc"
-     "PZGe"
-     "PZGy"
-     "Pl"
-     "PlR"
-     "Plpm"
-     "Plp"
-     "Plm"
-     "PlmR"
-     "PAl"
-     "PGnl"
-     "PAGnl"
-     "Pe"
-     "Pepm"
-     "Pemp"
-     "Pem"
-     "Pep"
-     "PGb"
-     "PGbm"
-     "PGbp"
-     "PGm"
-     "PGmpm"
-     "PGmm"
-     "PGmp"
-     "PGt"
-     "PGtpm"
-     "PGtm"
-     "PGtp"
-     "PGtpr"
-     "PGtprm"
-     "PGtprp"
-     "PGn"
-     "PGne"
-     "PGnGm"
-     "PGnGt"
-     "PGnGtpr"
-     "PAGn"
-     "PAGne"
-     "PAGnGm"
-     "PAGnGt"
-     "PAGnGtpr"
-     "PL"
-     "PLm"
-     "PLp"
-     "PLz"
-     "PLpm"
-     "PAL"
-     "PQq"
-     "PQd"
-     "PQu"
-     "PQs"
-     "PQc"
-     "PQb"
-     "PQt"
-     "PQbpr"
-     "PQtpr"
-     "PAQq"
-     "PAQd"
-     "PAQu"
-     "PAQs"
-     "PAQc"
-     "PAQb"
-     "PAQt"
-     "PAQbpr"
-     "PAQtpr"
-     "PQqb"
-     "PQqbpr"
-     "PQqc"
-     "PQqd"
-     "PQqdR"
-     "PQqs"
-     "PQqt"
-     "PQqtpr"
-     "PQqu"
-     "PQquR"
-     "PAQqb"
-     "PAQqbpr"
-     "PAQqc"
-     "PAQqd"
-     "PAQqs"
-     "PAQqt"
-     "PAQqtpr"
-     "PAQqu"
-     "Pp"
-     "Pn"
-     "PAp"
-     "PAn"
-     "PGa"
-     "PN"
-     "PNp"
-     "PNz"
-     "PGD"
-     "PGDpp"
-     "PGDp"
-     "PGDz"
-     "PGDm"
-     "PGL"
-     "PAGL"
-     "PGLc"
-     "PGLpc"
-     "PGLb"
-     "PGLzb"
-     "PGS"
-     "PAGS"
-     "PGSpm"
-     "PGSm"
-     "PGSp"
-     "PGSz"
-     "PGSstm"
-     "PGSstp"
-     "PGSstz"
-     "PGSc"
-     "PGSppc"
-     "PGSpc"
-     "PGSzc"
-     "PGSstc"
-     "PGSstppc"
-     "PGSstpc"
-     "PGSstzc"
-     "PGSb"
-     "PGSmb"
-     "PGSpb"
-     "PGSzb"
-     "PGSstb"
-     "PGSstmb"
-     "PGSstpb"
-     "PGSstzb"
-     "PGX"
-     "PAGX"
-     "PGXm"
-     "PGXz"
-     "PGXstm"
-     "PGXstz"
-     "PGXc"
-     "PGXpc"
-     "PGXzc"
-     "PGXprc"
-     "PGXprpc"
-     "PGXprzc"
-     "PGXstc"
-     "PGXstpc"
-     "PGXstzc"
-     "PGXcc"
-     "PGXpcc"
-     "PGXppcc"
-     "PGXstcc"
-     "PGXstpcc"
-     "PGXstppcc"
-     "PGXb"
-     "PGXmb"
-     "PGXzb"
-     "PGXprb"
-     "PGXprmb"
-     "PGXprzb"
-     "PGXstb"
-     "PGXstmb"
-     "PGXstzb"
-     "PGXbc"
-     "PGXpbc"
-     "PGXzbc"
-     "PGXprbc"
-     "PGXprpbc"
-     "PGXprzbc"
-     "PGXstbc"
-     "PGXstpbc"
-     "PGXstzbc"
-     "PGXbb"
-     "PGXmbb"
-     "PGXzbb"
-     "PGXstbb"
-     "PGXstmbb"
-     "PGXstzbb"
-     "PGO"
-     "PAGO"
-     "PGOm"
-     "PGOc"
-     "PGOzc"
-     "PGOstc"
-     "PGOstzc"
-     "PGOcc"
-     "PGOpcc"
-     "PGOstcc"
-     "PGOstpcc"
-     "PGOccc"
-     "PGOppccc"
-     "PGOb"
-     "PGOmb"
-     "PGOstb"
-     "PGOstmb"
-     "PGObc"
-     "PGOzbc"
-     "PGOprbc"
-     "PGOprzbc"
-     "PGOstbc"
-     "PGOstzbc"
-     "PGObcc"
-     "PGOpbcc"
-     "PGOstbcc"
-     "PGOstpbcc"
-     "PGObb"
-     "PGOmbb"
-     "PGOstbb"
-     "PGOstmbb"
-     "PGObbc"
-     "PGOzbbc"
-     "PGOstbbc"
-     "PGOstzbbc"
-     "PGObbb"
-     "PGOmbbb"
-     "PGT"
-     "PGTp"
-     "PGF"
-     "PGFmm"
-     "PGp"
-     "PGppm"
-     "PGpmp"
-     "PGpm"
-     "PGpp"
-     "PGpz"
-     "PGh"
-     "PGhpr"
-     "Pf"
-     "PGr"
-     "PGo"
-     "Pa"
-     "PGf"
-     "Ph"
-     "Pb"
-     "PK"
-     "PKpm"
-     "PKmp"
-     "PKm"
-     "PKp"
-     "PKL"
-     "PKS"
-     "PKst"
-     "PKz"
-     "PKzL"
-     "PKzS"
-     "PAK"
-     "PAKst"
-     "PAKz"
-     "PKeiii"
-     "PKgmiii"
-     "PKzeiii"
-     "PKzgmiii"
-     "PD"
-     "PDpm"
-     "PDmp"
-     "PDz"
-     "PDm"
-     "PDp"
-     "PDst"
-     "PDstm"
-     "PDstp"
-     "PDstpm"
-     "PAD"
-     "PADz"
-     "PDq"
-     "PDzq"
-     "PADq"
-     "PADzq"
-     "PDs"
-     "PDms"
-     "PDps"
-     "PDpms"
-     "PDstms"
-     "PDstps"
-     "PDstpms"
-     "PB"
-     "PBpm"
-     "PBp"
-     "PBm"
-     "PBz"
-     "PBst"
-     "PBstp"
-     "PBstz"
-     "PBstDz"
-     "PBstpDz"
-     "PBstzDz"
-     "PBstDo"
-     "PBstpDo"
-     "PBstzDo"
-     "PBstDt"
-     "PBstpDt"
-     "PBstzDt"
-     "PAB"
-     "PABz"
-     "PBs"
-     "PBzs"
-     "PABs"
-     "PABzs"
-     "PBsts"
-     "PBstzs"
-     "PBstsDz"
-     "PBstzsDz"
-     "PBstsDo"
-     "PBstzsDo"
-     "PBstsDt"
-     "PBstzsDt"
-     "PBq"
-     "PBzq"
-     "PABq"
-     "PABzq"
-     "PBd"
-     "PBzd"
-     "PABd"
-     "PABzd"
-     "PBu"
-     "PBzu"
-     "PABu"
-     "PABzu"
-     "PBc"
-     "PBmc"
-     "PBpc"
-     "PBpmc"
-     "PBstc"
-     "PBstpc"
-     "PBstcDz"
-     "PBstpcDz"
-     "PBstcDo"
-     "PBstpcDo"
-     "PBstcDt"
-     "PBstpcDt"
-     "PGhc"
-     "PJGy"
-     "PGy"
-     "PGc"
-     "PGcc"
-     "Phc"
-     "PX"
-     "PGhb"
-     "PGU"
-     "PGUpr"
-     "PGUprpr"
-     "PGUprprpr"
-     "PGUprprprpr"
-     "PGcb"
-     "PSH"
-     "PSHpm"
-     "PSHp"
-     "PSHm"
-     "PSHpmpm"
-     "PSHpp"
-     "PSHmm"
-     "PSh"
-     "PShz"
-     "PSA"
-     "PSAz"
-     "PSHzDo"
-     "PSHzDt"
-     "PSHzDT"
-     "PSGg"
-     "PSg"
-     "PSW"
-     "PSWm"
-     "PSWp"
-     "PSWpm"
-     "PSZ"
-     "PSZz"
-     "PSGc"
-     "PSGcz"
-     "PSGczDo"
-     "PSGczDt"
-     "PSGczDT"
-     "PSGczDf"
-     "PSGcm"
-     "PSGcp"
-     "PSGcpm"
-     "PSGcmDo"
-     "PSGcpDo"
-     "PSGcpmDo"
-     "PSGcmDt"
-     "PSGcpDt"
-     "PSGcpmDt"
-     "PSl"
-     "PASl"
-     "PSlL"
-     "PSlR"
-     "PSe"
-     "PSemL"
-     "PSemR"
-     "PSeL"
-     "PSeR"
-     "PSGm"
-     "PSGmmL"
-     "PSGmmR"
-     "PSGmL"
-     "PSGmR"
-     "PSGt"
-     "PSGtmDo"
-     "PSGtmDt"
-     "PSGtL"
-     "PSGtR"
-     "PSGtDo"
-     "PSGtDt"
-     "PSGn"
-     "PASGn"
-     "PSGne"
-     "PSGneL"
-     "PSGneR"
-     "PSGnGm"
-     "PSGnGmL"
-     "PSGnGmR"
-     "PSGnGt"
-     "PSGnGtDo"
-     "PSGnGtDt"
-     "PSQ"
-     "PSQL"
-     "PSQR"
-     "PSQd"
-     "PSQdL"
-     "PSQdR"
-     "PSQu"
-     "PSQuL"
-     "PSQuR"
-     "PSQs"
-     "PSQsL"
-     "PSQsR"
-     "PSQc"
-     "PSQcL"
-     "PSQcR"
-     "PSQb"
-     "PSQbL"
-     "PSQbR"
-     "PSQbDo"
-     "PSQbDt"
-     "PSQt"
-     "PSQtL"
-     "PSQtR"
-     "PSQtDo"
-     "PSQtDt"
-     "PASQ"
-     "PASQL"
-     "PASQR"
-     "PASQd"
-     "PASQdL"
-     "PASQdR"
-     "PASQu"
-     "PASQuL"
-     "PASQuR"
-     "PASQs"
-     "PASQsL"
-     "PASQsR"
-     "PASQc"
-     "PASQcL"
-     "PASQcR"
-     "PASQb"
-     "PASQbL"
-     "PASQbR"
-     "PASQbDo"
-     "PASQbDt"
-     "PASQt"
-     "PASQtL"
-     "PASQtR"
-     "PASQtDo"
-     "PASQtDt"
-     "PEz"
-     "PgA"
-     "PXXA"
-     "PXXAz"
-     "PXXG"
-     "PXXSG"
-     "UP"
-     "upalpha"
-     "upbeta"
-     "upgamma"
-     "updelta"
-     "upepsilon"
-     "upzeta"
-     "upeta"
-     "uptheta"
-     "upiota"
-     "upkappa"
-     "uplambda"
-     "upmu"
-     "upnu"
-     "upxi"
-     "uppi"
-     "uprho"
-     "upsigma"
-     "uptau"
-     "upupsilon"
-     "upphi"
-     "upchi"
-     "uppsi"
-     "upomega"
-     "upvartheta"
-     "upvarpi"
-     "Upgamma"
-     "Updelta"
-     "Uptheta"
-     "Uplambda"
-     "Upxi"
-     "Uppi"
-     "Upsigma"
-     "Upupsilon"
-     "Upphi"
-     "Uppsi"
-     "Upomega"
-     "upvarepsilon"
-     "upvarrho"
-     "upvarsigma"
+     '("upvarphi" 0)
+     '("Pg" 0)
+     '("PGg" 0)
+     '("PW" 0)
+     '("PWpm" 0)
+     '("PWmp" 0)
+     '("PWp" 0)
+     '("PWm" 0)
+     '("PZ" 0)
+     '("PZz" 0)
+     '("PH" 0)
+     '("PHz" 0)
+     '("PWDt" 0)
+     '("PWpDt" 0)
+     '("PWpr" 0)
+     '("PWL" 0)
+     '("PWR" 0)
+     '("PZpr" 0)
+     '("PZprpr" 0)
+     '("PZst" 0)
+     '("PZzDt" 0)
+     '("PZzDT" 0)
+     '("PZL" 0)
+     '("PZLR" 0)
+     '("PZR" 0)
+     '("PZGc" 0)
+     '("PZGe" 0)
+     '("PZGy" 0)
+     '("Pl" 0)
+     '("PlR" 0)
+     '("Plpm" 0)
+     '("Plp" 0)
+     '("Plm" 0)
+     '("PlmR" 0)
+     '("PAl" 0)
+     '("PGnl" 0)
+     '("PAGnl" 0)
+     '("Pe" 0)
+     '("Pepm" 0)
+     '("Pemp" 0)
+     '("Pem" 0)
+     '("Pep" 0)
+     '("PGb" 0)
+     '("PGbm" 0)
+     '("PGbp" 0)
+     '("PGm" 0)
+     '("PGmpm" 0)
+     '("PGmm" 0)
+     '("PGmp" 0)
+     '("PGt" 0)
+     '("PGtpm" 0)
+     '("PGtm" 0)
+     '("PGtp" 0)
+     '("PGtpr" 0)
+     '("PGtprm" 0)
+     '("PGtprp" 0)
+     '("PGn" 0)
+     '("PGne" 0)
+     '("PGnGm" 0)
+     '("PGnGt" 0)
+     '("PGnGtpr" 0)
+     '("PAGn" 0)
+     '("PAGne" 0)
+     '("PAGnGm" 0)
+     '("PAGnGt" 0)
+     '("PAGnGtpr" 0)
+     '("PL" 0)
+     '("PLm" 0)
+     '("PLp" 0)
+     '("PLz" 0)
+     '("PLpm" 0)
+     '("PAL" 0)
+     '("PQq" 0)
+     '("PQd" 0)
+     '("PQu" 0)
+     '("PQs" 0)
+     '("PQc" 0)
+     '("PQb" 0)
+     '("PQt" 0)
+     '("PQbpr" 0)
+     '("PQtpr" 0)
+     '("PAQq" 0)
+     '("PAQd" 0)
+     '("PAQu" 0)
+     '("PAQs" 0)
+     '("PAQc" 0)
+     '("PAQb" 0)
+     '("PAQt" 0)
+     '("PAQbpr" 0)
+     '("PAQtpr" 0)
+     '("PQqb" 0)
+     '("PQqbpr" 0)
+     '("PQqc" 0)
+     '("PQqd" 0)
+     '("PQqdR" 0)
+     '("PQqs" 0)
+     '("PQqt" 0)
+     '("PQqtpr" 0)
+     '("PQqu" 0)
+     '("PQquR" 0)
+     '("PAQqb" 0)
+     '("PAQqbpr" 0)
+     '("PAQqc" 0)
+     '("PAQqd" 0)
+     '("PAQqs" 0)
+     '("PAQqt" 0)
+     '("PAQqtpr" 0)
+     '("PAQqu" 0)
+     '("Pp" 0)
+     '("Pn" 0)
+     '("PAp" 0)
+     '("PAn" 0)
+     '("PGa" 0)
+     '("PN" 0)
+     '("PNp" 0)
+     '("PNz" 0)
+     '("PGD" 0)
+     '("PGDpp" 0)
+     '("PGDp" 0)
+     '("PGDz" 0)
+     '("PGDm" 0)
+     '("PGL" 0)
+     '("PAGL" 0)
+     '("PGLc" 0)
+     '("PGLpc" 0)
+     '("PGLb" 0)
+     '("PGLzb" 0)
+     '("PGS" 0)
+     '("PAGS" 0)
+     '("PGSpm" 0)
+     '("PGSm" 0)
+     '("PGSp" 0)
+     '("PGSz" 0)
+     '("PGSstm" 0)
+     '("PGSstp" 0)
+     '("PGSstz" 0)
+     '("PGSc" 0)
+     '("PGSppc" 0)
+     '("PGSpc" 0)
+     '("PGSzc" 0)
+     '("PGSstc" 0)
+     '("PGSstppc" 0)
+     '("PGSstpc" 0)
+     '("PGSstzc" 0)
+     '("PGSb" 0)
+     '("PGSmb" 0)
+     '("PGSpb" 0)
+     '("PGSzb" 0)
+     '("PGSstb" 0)
+     '("PGSstmb" 0)
+     '("PGSstpb" 0)
+     '("PGSstzb" 0)
+     '("PGX" 0)
+     '("PAGX" 0)
+     '("PGXm" 0)
+     '("PGXz" 0)
+     '("PGXstm" 0)
+     '("PGXstz" 0)
+     '("PGXc" 0)
+     '("PGXpc" 0)
+     '("PGXzc" 0)
+     '("PGXprc" 0)
+     '("PGXprpc" 0)
+     '("PGXprzc" 0)
+     '("PGXstc" 0)
+     '("PGXstpc" 0)
+     '("PGXstzc" 0)
+     '("PGXcc" 0)
+     '("PGXpcc" 0)
+     '("PGXppcc" 0)
+     '("PGXstcc" 0)
+     '("PGXstpcc" 0)
+     '("PGXstppcc" 0)
+     '("PGXb" 0)
+     '("PGXmb" 0)
+     '("PGXzb" 0)
+     '("PGXprb" 0)
+     '("PGXprmb" 0)
+     '("PGXprzb" 0)
+     '("PGXstb" 0)
+     '("PGXstmb" 0)
+     '("PGXstzb" 0)
+     '("PGXbc" 0)
+     '("PGXpbc" 0)
+     '("PGXzbc" 0)
+     '("PGXprbc" 0)
+     '("PGXprpbc" 0)
+     '("PGXprzbc" 0)
+     '("PGXstbc" 0)
+     '("PGXstpbc" 0)
+     '("PGXstzbc" 0)
+     '("PGXbb" 0)
+     '("PGXmbb" 0)
+     '("PGXzbb" 0)
+     '("PGXstbb" 0)
+     '("PGXstmbb" 0)
+     '("PGXstzbb" 0)
+     '("PGO" 0)
+     '("PAGO" 0)
+     '("PGOm" 0)
+     '("PGOc" 0)
+     '("PGOzc" 0)
+     '("PGOstc" 0)
+     '("PGOstzc" 0)
+     '("PGOcc" 0)
+     '("PGOpcc" 0)
+     '("PGOstcc" 0)
+     '("PGOstpcc" 0)
+     '("PGOccc" 0)
+     '("PGOppccc" 0)
+     '("PGOb" 0)
+     '("PGOmb" 0)
+     '("PGOstb" 0)
+     '("PGOstmb" 0)
+     '("PGObc" 0)
+     '("PGOzbc" 0)
+     '("PGOprbc" 0)
+     '("PGOprzbc" 0)
+     '("PGOstbc" 0)
+     '("PGOstzbc" 0)
+     '("PGObcc" 0)
+     '("PGOpbcc" 0)
+     '("PGOstbcc" 0)
+     '("PGOstpbcc" 0)
+     '("PGObb" 0)
+     '("PGOmbb" 0)
+     '("PGOstbb" 0)
+     '("PGOstmbb" 0)
+     '("PGObbc" 0)
+     '("PGOzbbc" 0)
+     '("PGOstbbc" 0)
+     '("PGOstzbbc" 0)
+     '("PGObbb" 0)
+     '("PGOmbbb" 0)
+     '("PGT" 0)
+     '("PGTp" 0)
+     '("PGF" 0)
+     '("PGFmm" 0)
+     '("PGp" 0)
+     '("PGppm" 0)
+     '("PGpmp" 0)
+     '("PGpm" 0)
+     '("PGpp" 0)
+     '("PGpz" 0)
+     '("PGh" 0)
+     '("PGhpr" 0)
+     '("Pf" 0)
+     '("PGr" 0)
+     '("PGo" 0)
+     '("Pa" 0)
+     '("PGf" 0)
+     '("Ph" 0)
+     '("Pb" 0)
+     '("PK" 0)
+     '("PKpm" 0)
+     '("PKmp" 0)
+     '("PKm" 0)
+     '("PKp" 0)
+     '("PKL" 0)
+     '("PKS" 0)
+     '("PKst" 0)
+     '("PKz" 0)
+     '("PKzL" 0)
+     '("PKzS" 0)
+     '("PAK" 0)
+     '("PAKst" 0)
+     '("PAKz" 0)
+     '("PKeiii" 0)
+     '("PKgmiii" 0)
+     '("PKzeiii" 0)
+     '("PKzgmiii" 0)
+     '("PD" 0)
+     '("PDpm" 0)
+     '("PDmp" 0)
+     '("PDz" 0)
+     '("PDm" 0)
+     '("PDp" 0)
+     '("PDst" 0)
+     '("PDstm" 0)
+     '("PDstp" 0)
+     '("PDstpm" 0)
+     '("PAD" 0)
+     '("PADz" 0)
+     '("PDq" 0)
+     '("PDzq" 0)
+     '("PADq" 0)
+     '("PADzq" 0)
+     '("PDs" 0)
+     '("PDms" 0)
+     '("PDps" 0)
+     '("PDpms" 0)
+     '("PDstms" 0)
+     '("PDstps" 0)
+     '("PDstpms" 0)
+     '("PB" 0)
+     '("PBpm" 0)
+     '("PBp" 0)
+     '("PBm" 0)
+     '("PBz" 0)
+     '("PBst" 0)
+     '("PBstp" 0)
+     '("PBstz" 0)
+     '("PBstDz" 0)
+     '("PBstpDz" 0)
+     '("PBstzDz" 0)
+     '("PBstDo" 0)
+     '("PBstpDo" 0)
+     '("PBstzDo" 0)
+     '("PBstDt" 0)
+     '("PBstpDt" 0)
+     '("PBstzDt" 0)
+     '("PAB" 0)
+     '("PABz" 0)
+     '("PBs" 0)
+     '("PBzs" 0)
+     '("PABs" 0)
+     '("PABzs" 0)
+     '("PBsts" 0)
+     '("PBstzs" 0)
+     '("PBstsDz" 0)
+     '("PBstzsDz" 0)
+     '("PBstsDo" 0)
+     '("PBstzsDo" 0)
+     '("PBstsDt" 0)
+     '("PBstzsDt" 0)
+     '("PBq" 0)
+     '("PBzq" 0)
+     '("PABq" 0)
+     '("PABzq" 0)
+     '("PBd" 0)
+     '("PBzd" 0)
+     '("PABd" 0)
+     '("PABzd" 0)
+     '("PBu" 0)
+     '("PBzu" 0)
+     '("PABu" 0)
+     '("PABzu" 0)
+     '("PBc" 0)
+     '("PBmc" 0)
+     '("PBpc" 0)
+     '("PBpmc" 0)
+     '("PBstc" 0)
+     '("PBstpc" 0)
+     '("PBstcDz" 0)
+     '("PBstpcDz" 0)
+     '("PBstcDo" 0)
+     '("PBstpcDo" 0)
+     '("PBstcDt" 0)
+     '("PBstpcDt" 0)
+     '("PGhc" 0)
+     '("PJGy" 0)
+     '("PGy" 0)
+     '("PGc" 0)
+     '("PGcc" 0)
+     '("Phc" 0)
+     '("PX" 0)
+     '("PGhb" 0)
+     '("PGU" 0)
+     '("PGUpr" 0)
+     '("PGUprpr" 0)
+     '("PGUprprpr" 0)
+     '("PGUprprprpr" 0)
+     '("PGcb" 0)
+     '("PSH" 0)
+     '("PSHpm" 0)
+     '("PSHp" 0)
+     '("PSHm" 0)
+     '("PSHpmpm" 0)
+     '("PSHpp" 0)
+     '("PSHmm" 0)
+     '("PSh" 0)
+     '("PShz" 0)
+     '("PSA" 0)
+     '("PSAz" 0)
+     '("PSHzDo" 0)
+     '("PSHzDt" 0)
+     '("PSHzDT" 0)
+     '("PSGg" 0)
+     '("PSg" 0)
+     '("PSW" 0)
+     '("PSWm" 0)
+     '("PSWp" 0)
+     '("PSWpm" 0)
+     '("PSZ" 0)
+     '("PSZz" 0)
+     '("PSGc" 0)
+     '("PSGcz" 0)
+     '("PSGczDo" 0)
+     '("PSGczDt" 0)
+     '("PSGczDT" 0)
+     '("PSGczDf" 0)
+     '("PSGcm" 0)
+     '("PSGcp" 0)
+     '("PSGcpm" 0)
+     '("PSGcmDo" 0)
+     '("PSGcpDo" 0)
+     '("PSGcpmDo" 0)
+     '("PSGcmDt" 0)
+     '("PSGcpDt" 0)
+     '("PSGcpmDt" 0)
+     '("PSl" 0)
+     '("PASl" 0)
+     '("PSlL" 0)
+     '("PSlR" 0)
+     '("PSe" 0)
+     '("PSemL" 0)
+     '("PSemR" 0)
+     '("PSeL" 0)
+     '("PSeR" 0)
+     '("PSGm" 0)
+     '("PSGmmL" 0)
+     '("PSGmmR" 0)
+     '("PSGmL" 0)
+     '("PSGmR" 0)
+     '("PSGt" 0)
+     '("PSGtmDo" 0)
+     '("PSGtmDt" 0)
+     '("PSGtL" 0)
+     '("PSGtR" 0)
+     '("PSGtDo" 0)
+     '("PSGtDt" 0)
+     '("PSGn" 0)
+     '("PASGn" 0)
+     '("PSGne" 0)
+     '("PSGneL" 0)
+     '("PSGneR" 0)
+     '("PSGnGm" 0)
+     '("PSGnGmL" 0)
+     '("PSGnGmR" 0)
+     '("PSGnGt" 0)
+     '("PSGnGtDo" 0)
+     '("PSGnGtDt" 0)
+     '("PSQ" 0)
+     '("PSQL" 0)
+     '("PSQR" 0)
+     '("PSQd" 0)
+     '("PSQdL" 0)
+     '("PSQdR" 0)
+     '("PSQu" 0)
+     '("PSQuL" 0)
+     '("PSQuR" 0)
+     '("PSQs" 0)
+     '("PSQsL" 0)
+     '("PSQsR" 0)
+     '("PSQc" 0)
+     '("PSQcL" 0)
+     '("PSQcR" 0)
+     '("PSQb" 0)
+     '("PSQbL" 0)
+     '("PSQbR" 0)
+     '("PSQbDo" 0)
+     '("PSQbDt" 0)
+     '("PSQt" 0)
+     '("PSQtL" 0)
+     '("PSQtR" 0)
+     '("PSQtDo" 0)
+     '("PSQtDt" 0)
+     '("PASQ" 0)
+     '("PASQL" 0)
+     '("PASQR" 0)
+     '("PASQd" 0)
+     '("PASQdL" 0)
+     '("PASQdR" 0)
+     '("PASQu" 0)
+     '("PASQuL" 0)
+     '("PASQuR" 0)
+     '("PASQs" 0)
+     '("PASQsL" 0)
+     '("PASQsR" 0)
+     '("PASQc" 0)
+     '("PASQcL" 0)
+     '("PASQcR" 0)
+     '("PASQb" 0)
+     '("PASQbL" 0)
+     '("PASQbR" 0)
+     '("PASQbDo" 0)
+     '("PASQbDt" 0)
+     '("PASQt" 0)
+     '("PASQtL" 0)
+     '("PASQtR" 0)
+     '("PASQtDo" 0)
+     '("PASQtDt" 0)
+     '("PEz" 0)
+     '("PgA" 0)
+     '("PXXA" 0)
+     '("PXXAz" 0)
+     '("PXXG" 0)
+     '("PXXSG" 0)
+     '("UP" 0)
+     '("upalpha" 0)
+     '("upbeta" 0)
+     '("upgamma" 0)
+     '("updelta" 0)
+     '("upepsilon" 0)
+     '("upzeta" 0)
+     '("upeta" 0)
+     '("uptheta" 0)
+     '("upiota" 0)
+     '("upkappa" 0)
+     '("uplambda" 0)
+     '("upmu" 0)
+     '("upnu" 0)
+     '("upxi" 0)
+     '("uppi" 0)
+     '("uprho" 0)
+     '("upsigma" 0)
+     '("uptau" 0)
+     '("upupsilon" 0)
+     '("upphi" 0)
+     '("upchi" 0)
+     '("uppsi" 0)
+     '("upomega" 0)
+     '("upvartheta" 0)
+     '("upvarpi" 0)
+     '("Upgamma" 0)
+     '("Updelta" 0)
+     '("Uptheta" 0)
+     '("Uplambda" 0)
+     '("Upxi" 0)
+     '("Uppi" 0)
+     '("Upsigma" 0)
+     '("Upupsilon" 0)
+     '("Upphi" 0)
+     '("Uppsi" 0)
+     '("Upomega" 0)
+     '("upvarepsilon" 0)
+     '("upvarrho" 0)
+     '("upvarsigma" 0)
      "Xspace")
     (TeX-run-style-hooks
      "amsmath"
      "xspace"
      "hepparticles")))
-
