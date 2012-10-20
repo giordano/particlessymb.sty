f6e641b00f96e6ea90603d9a86a8921704ad9ee0
Mosè Giordano <giordano.mose@libero.it>
2012-10-20 23:19:06 +0200

    AUCTeX style file for LaTeX package `particlessymb.sty' (v1.4)
    This file has been auto generated with `TeX-auto-generate' function.

---
+++ particlessymb.el
@@ -0,0 +1,682 @@
+(TeX-add-style-hook "particlessymb"
+ (lambda ()
+    (TeX-add-symbols
+     '("PGcbDtP" 1)
+     '("PGcbDoP" 1)
+     '("PGcbDzP" 1)
+     '("PGUP" 1)
+     '("PGhbP" 1)
+     '("PXP" 1)
+     '("PhcP" 1)
+     '("PGccDtP" 1)
+     '("PGccDoP" 1)
+     '("PGccDzP" 1)
+     '("PGyP" 1)
+     '("PJGyP" 1)
+     '("PGhcP" 1)
+     '("PBpcDoP" 1)
+     '("PBcDoP" 1)
+     '("PBzsDoP" 1)
+     '("PBsDoP" 1)
+     '("PBstzsJP" 1)
+     '("PBstsJP" 1)
+     '("PBzDoP" 1)
+     '("PBpDoP" 1)
+     '("PBDoP" 1)
+     '("PBstzJP" 1)
+     '("PBstpJP" 1)
+     '("PBstJP" 1)
+     '("PDpsDtP" 1)
+     '("PDmsDtP" 1)
+     '("PDpmsDtP" 1)
+     '("PDpsDoP" 1)
+     '("PDmsDoP" 1)
+     '("PDpmsDoP" 1)
+     '("PDpsJP" 1)
+     '("PDmsJP" 1)
+     '("PDpmsJP" 1)
+     '("PDstpsJP" 1)
+     '("PDstmsJP" 1)
+     '("PDstpmsJP" 1)
+     '("PDstpDtP" 1)
+     '("PDstmDtP" 1)
+     '("PDstpmDtP" 1)
+     '("PDstzDtP" 1)
+     '("PDpDoP" 1)
+     '("PDmDoP" 1)
+     '("PDpmDoP" 1)
+     '("PDzDoP" 1)
+     '("PDstpP" 1)
+     '("PDstmP" 1)
+     '("PDstpmP" 1)
+     '("PDstzP" 1)
+     '("PKstDFP" 1)
+     '("PKstDfP" 1)
+     '("PKstDTP" 1)
+     '("PKstDtP" 1)
+     '("PKstDoP" 1)
+     '("PKstDzP" 1)
+     '("PKstP" 1)
+     '("PKDfP" 1)
+     '("PKDTP" 1)
+     '("PKDtP" 1)
+     '("PKDoP" 1)
+     '("PKDzP" 1)
+     '("PKP" 1)
+     '("PbzDoP" 1)
+     '("PbpDoP" 1)
+     '("PbDoP" 1)
+     '("PhDoP" 1)
+     '("PGfDTP" 1)
+     '("PGfP" 1)
+     '("PazDsP" 1)
+     '("PapDsP" 1)
+     '("PaDsP" 1)
+     '("PazDfP" 1)
+     '("PapDfP" 1)
+     '("PaDfP" 1)
+     '("PazDtP" 1)
+     '("PapDtP" 1)
+     '("PaDtP" 1)
+     '("PazDoP" 1)
+     '("PapDoP" 1)
+     '("PaDoP" 1)
+     '("PazDzP" 1)
+     '("PapDzP" 1)
+     '("PaDzP" 1)
+     '("PGoDTP" 1)
+     '("PGoP" 1)
+     '("PGrzDFP" 1)
+     '("PGrpDFP" 1)
+     '("PGrDFP" 1)
+     '("PGrzDTP" 1)
+     '("PGrpDTP" 1)
+     '("PGrDTP" 1)
+     '("PGrzP" 1)
+     '("PGrpP" 1)
+     '("PGrP" 1)
+     '("PfJP" 1)
+     '("PfDsP" 1)
+     '("PfDfP" 1)
+     '("PfprDtP" 1)
+     '("PfDtP" 1)
+     '("PfDoP" 1)
+     '("PfDzP" 1)
+     '("PGhDtP" 1)
+     '("PGhprP" 1)
+     '("PGhP" 1)
+     '("PGpzDtP" 1)
+     '("PGppDtP" 1)
+     '("PGpDtP" 1)
+     '("PGpzDoP" 1)
+     '("PGppDoP" 1)
+     '("PGpDoP" 1)
+     '("PGpzP" 1)
+     '("PGppP" 1)
+     '("PGpP" 1)
+     '("PGOmP" 1)
+     '("PGOP" 1)
+     '("PGXcP" 1)
+     '("PGXstzP" 1)
+     '("PGXstmP" 1)
+     '("PGXzP" 1)
+     '("PGXmP" 1)
+     '("PGXP" 1)
+     '("PGSstzcP" 1)
+     '("PGSstpcP" 1)
+     '("PGSstppcP" 1)
+     '("PGSstcP" 1)
+     '("PGSzcP" 1)
+     '("PGSpcP" 1)
+     '("PGSppcP" 1)
+     '("PGScP" 1)
+     '("PGSzP" 1)
+     '("PGSpP" 1)
+     '("PGSmP" 1)
+     '("PGSP" 1)
+     '("PGLpcP" 1)
+     '("PGLP" 1)
+     '("PGFP" 1)
+     '("PGQpP" 1)
+     '("PGDmP" 1)
+     '("PGDzP" 1)
+     '("PGDpP" 1)
+     '("PGDppP" 1)
+     '("PGDP" 1)
+     '("PNzP" 1)
+     '("PNpP" 1)
+     '("PNP" 1)
+     "upvarphi"
+     "Pg"
+     "PGg"
+     "PW"
+     "PWpm"
+     "PWmp"
+     "PWp"
+     "PWm"
+     "PZ"
+     "PZz"
+     "PH"
+     "PHz"
+     "PWDt"
+     "PWpDt"
+     "PWpr"
+     "PWL"
+     "PWR"
+     "PZpr"
+     "PZprpr"
+     "PZst"
+     "PZzDt"
+     "PZzDT"
+     "PZL"
+     "PZLR"
+     "PZR"
+     "PZGc"
+     "PZGe"
+     "PZGy"
+     "Pl"
+     "PlR"
+     "Plpm"
+     "Plp"
+     "Plm"
+     "PlmR"
+     "PAl"
+     "PGnl"
+     "PAGnl"
+     "Pe"
+     "Pepm"
+     "Pemp"
+     "Pem"
+     "Pep"
+     "PGb"
+     "PGbm"
+     "PGbp"
+     "PGm"
+     "PGmpm"
+     "PGmm"
+     "PGmp"
+     "PGt"
+     "PGtpm"
+     "PGtm"
+     "PGtp"
+     "PGtpr"
+     "PGtprm"
+     "PGtprp"
+     "PGn"
+     "PGne"
+     "PGnGm"
+     "PGnGt"
+     "PGnGtpr"
+     "PAGn"
+     "PAGne"
+     "PAGnGm"
+     "PAGnGt"
+     "PAGnGtpr"
+     "PL"
+     "PLm"
+     "PLp"
+     "PLz"
+     "PLpm"
+     "PAL"
+     "PQq"
+     "PQd"
+     "PQu"
+     "PQs"
+     "PQc"
+     "PQb"
+     "PQt"
+     "PQbpr"
+     "PQtpr"
+     "PAQq"
+     "PAQd"
+     "PAQu"
+     "PAQs"
+     "PAQc"
+     "PAQb"
+     "PAQt"
+     "PAQbpr"
+     "PAQtpr"
+     "PQqb"
+     "PQqbpr"
+     "PQqc"
+     "PQqd"
+     "PQqdR"
+     "PQqs"
+     "PQqt"
+     "PQqtpr"
+     "PQqu"
+     "PQquR"
+     "PAQqb"
+     "PAQqbpr"
+     "PAQqc"
+     "PAQqd"
+     "PAQqs"
+     "PAQqt"
+     "PAQqtpr"
+     "PAQqu"
+     "Pp"
+     "Pn"
+     "PAp"
+     "PAn"
+     "PGa"
+     "PN"
+     "PNp"
+     "PNz"
+     "PGD"
+     "PGDpp"
+     "PGDp"
+     "PGDz"
+     "PGDm"
+     "PGL"
+     "PAGL"
+     "PGLc"
+     "PGLpc"
+     "PGLb"
+     "PGLzb"
+     "PGS"
+     "PAGS"
+     "PGSpm"
+     "PGSm"
+     "PGSp"
+     "PGSz"
+     "PGSstm"
+     "PGSstp"
+     "PGSstz"
+     "PGSc"
+     "PGSppc"
+     "PGSpc"
+     "PGSzc"
+     "PGSstc"
+     "PGSstppc"
+     "PGSstpc"
+     "PGSstzc"
+     "PGSb"
+     "PGSmb"
+     "PGSpb"
+     "PGSzb"
+     "PGSstb"
+     "PGSstmb"
+     "PGSstpb"
+     "PGSstzb"
+     "PGX"
+     "PAGX"
+     "PGXm"
+     "PGXz"
+     "PGXstm"
+     "PGXstz"
+     "PGXc"
+     "PGXpc"
+     "PGXzc"
+     "PGXprc"
+     "PGXprpc"
+     "PGXprzc"
+     "PGXstc"
+     "PGXstpc"
+     "PGXstzc"
+     "PGXcc"
+     "PGXpcc"
+     "PGXppcc"
+     "PGXstcc"
+     "PGXstpcc"
+     "PGXstppcc"
+     "PGXb"
+     "PGXmb"
+     "PGXzb"
+     "PGXprb"
+     "PGXprmb"
+     "PGXprzb"
+     "PGXstb"
+     "PGXstmb"
+     "PGXstzb"
+     "PGXbc"
+     "PGXpbc"
+     "PGXzbc"
+     "PGXprbc"
+     "PGXprpbc"
+     "PGXprzbc"
+     "PGXstbc"
+     "PGXstpbc"
+     "PGXstzbc"
+     "PGXbb"
+     "PGXmbb"
+     "PGXzbb"
+     "PGXstbb"
+     "PGXstmbb"
+     "PGXstzbb"
+     "PGO"
+     "PAGO"
+     "PGOm"
+     "PGOc"
+     "PGOzc"
+     "PGOstc"
+     "PGOstzc"
+     "PGOcc"
+     "PGOpcc"
+     "PGOstcc"
+     "PGOstpcc"
+     "PGOccc"
+     "PGOppccc"
+     "PGOb"
+     "PGOmb"
+     "PGOstb"
+     "PGOstmb"
+     "PGObc"
+     "PGOzbc"
+     "PGOprbc"
+     "PGOprzbc"
+     "PGOstbc"
+     "PGOstzbc"
+     "PGObcc"
+     "PGOpbcc"
+     "PGOstbcc"
+     "PGOstpbcc"
+     "PGObb"
+     "PGOmbb"
+     "PGOstbb"
+     "PGOstmbb"
+     "PGObbc"
+     "PGOzbbc"
+     "PGOstbbc"
+     "PGOstzbbc"
+     "PGObbb"
+     "PGOmbbb"
+     "PGT"
+     "PGTp"
+     "PGF"
+     "PGFmm"
+     "PGp"
+     "PGppm"
+     "PGpmp"
+     "PGpm"
+     "PGpp"
+     "PGpz"
+     "PGh"
+     "PGhpr"
+     "Pf"
+     "PGr"
+     "PGo"
+     "Pa"
+     "PGf"
+     "Ph"
+     "Pb"
+     "PK"
+     "PKpm"
+     "PKmp"
+     "PKm"
+     "PKp"
+     "PKL"
+     "PKS"
+     "PKst"
+     "PKz"
+     "PKzL"
+     "PKzS"
+     "PAK"
+     "PAKst"
+     "PAKz"
+     "PKeiii"
+     "PKgmiii"
+     "PKzeiii"
+     "PKzgmiii"
+     "PD"
+     "PDpm"
+     "PDmp"
+     "PDz"
+     "PDm"
+     "PDp"
+     "PDst"
+     "PDstm"
+     "PDstp"
+     "PDstpm"
+     "PAD"
+     "PADz"
+     "PDq"
+     "PDzq"
+     "PADq"
+     "PADzq"
+     "PDs"
+     "PDms"
+     "PDps"
+     "PDpms"
+     "PDstms"
+     "PDstps"
+     "PDstpms"
+     "PB"
+     "PBpm"
+     "PBp"
+     "PBm"
+     "PBz"
+     "PBst"
+     "PBstp"
+     "PBstz"
+     "PBstDz"
+     "PBstpDz"
+     "PBstzDz"
+     "PBstDo"
+     "PBstpDo"
+     "PBstzDo"
+     "PBstDt"
+     "PBstpDt"
+     "PBstzDt"
+     "PAB"
+     "PABz"
+     "PBs"
+     "PBzs"
+     "PABs"
+     "PABzs"
+     "PBsts"
+     "PBstzs"
+     "PBstsDz"
+     "PBstzsDz"
+     "PBstsDo"
+     "PBstzsDo"
+     "PBstsDt"
+     "PBstzsDt"
+     "PBq"
+     "PBzq"
+     "PABq"
+     "PABzq"
+     "PBd"
+     "PBzd"
+     "PABd"
+     "PABzd"
+     "PBu"
+     "PBzu"
+     "PABu"
+     "PABzu"
+     "PBc"
+     "PBmc"
+     "PBpc"
+     "PBpmc"
+     "PBstc"
+     "PBstpc"
+     "PBstcDz"
+     "PBstpcDz"
+     "PBstcDo"
+     "PBstpcDo"
+     "PBstcDt"
+     "PBstpcDt"
+     "PGhc"
+     "PJGy"
+     "PGy"
+     "PGc"
+     "PGcc"
+     "Phc"
+     "PX"
+     "PGhb"
+     "PGU"
+     "PGUpr"
+     "PGUprpr"
+     "PGUprprpr"
+     "PGUprprprpr"
+     "PGcb"
+     "PSH"
+     "PSHpm"
+     "PSHp"
+     "PSHm"
+     "PSHpmpm"
+     "PSHpp"
+     "PSHmm"
+     "PSh"
+     "PShz"
+     "PSA"
+     "PSAz"
+     "PSHzDo"
+     "PSHzDt"
+     "PSHzDT"
+     "PSGg"
+     "PSg"
+     "PSW"
+     "PSWm"
+     "PSWp"
+     "PSWpm"
+     "PSZ"
+     "PSZz"
+     "PSGc"
+     "PSGcz"
+     "PSGczDo"
+     "PSGczDt"
+     "PSGczDT"
+     "PSGczDf"
+     "PSGcm"
+     "PSGcp"
+     "PSGcpm"
+     "PSGcmDo"
+     "PSGcpDo"
+     "PSGcpmDo"
+     "PSGcmDt"
+     "PSGcpDt"
+     "PSGcpmDt"
+     "PSl"
+     "PASl"
+     "PSlL"
+     "PSlR"
+     "PSe"
+     "PSemL"
+     "PSemR"
+     "PSeL"
+     "PSeR"
+     "PSGm"
+     "PSGmmL"
+     "PSGmmR"
+     "PSGmL"
+     "PSGmR"
+     "PSGt"
+     "PSGtmDo"
+     "PSGtmDt"
+     "PSGtL"
+     "PSGtR"
+     "PSGtDo"
+     "PSGtDt"
+     "PSGn"
+     "PASGn"
+     "PSGne"
+     "PSGneL"
+     "PSGneR"
+     "PSGnGm"
+     "PSGnGmL"
+     "PSGnGmR"
+     "PSGnGt"
+     "PSGnGtDo"
+     "PSGnGtDt"
+     "PSQ"
+     "PSQL"
+     "PSQR"
+     "PSQd"
+     "PSQdL"
+     "PSQdR"
+     "PSQu"
+     "PSQuL"
+     "PSQuR"
+     "PSQs"
+     "PSQsL"
+     "PSQsR"
+     "PSQc"
+     "PSQcL"
+     "PSQcR"
+     "PSQb"
+     "PSQbL"
+     "PSQbR"
+     "PSQbDo"
+     "PSQbDt"
+     "PSQt"
+     "PSQtL"
+     "PSQtR"
+     "PSQtDo"
+     "PSQtDt"
+     "PASQ"
+     "PASQL"
+     "PASQR"
+     "PASQd"
+     "PASQdL"
+     "PASQdR"
+     "PASQu"
+     "PASQuL"
+     "PASQuR"
+     "PASQs"
+     "PASQsL"
+     "PASQsR"
+     "PASQc"
+     "PASQcL"
+     "PASQcR"
+     "PASQb"
+     "PASQbL"
+     "PASQbR"
+     "PASQbDo"
+     "PASQbDt"
+     "PASQt"
+     "PASQtL"
+     "PASQtR"
+     "PASQtDo"
+     "PASQtDt"
+     "PEz"
+     "PgA"
+     "PXXA"
+     "PXXAz"
+     "PXXG"
+     "PXXSG"
+     "UP"
+     "upalpha"
+     "upbeta"
+     "upgamma"
+     "updelta"
+     "upepsilon"
+     "upzeta"
+     "upeta"
+     "uptheta"
+     "upiota"
+     "upkappa"
+     "uplambda"
+     "upmu"
+     "upnu"
+     "upxi"
+     "uppi"
+     "uprho"
+     "upsigma"
+     "uptau"
+     "upupsilon"
+     "upphi"
+     "upchi"
+     "uppsi"
+     "upomega"
+     "upvartheta"
+     "upvarpi"
+     "Upgamma"
+     "Updelta"
+     "Uptheta"
+     "Uplambda"
+     "Upxi"
+     "Uppi"
+     "Upsigma"
+     "Upupsilon"
+     "Upphi"
+     "Uppsi"
+     "Upomega"
+     "upvarepsilon"
+     "upvarrho"
+     "upvarsigma"
+     "Xspace")
+    (TeX-run-style-hooks
+     "amsmath"
+     "xspace"
+     "hepparticles")))
+
